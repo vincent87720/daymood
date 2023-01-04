2dcb590b901f0a9984b5f3f7bec6f45dcc3f0198
vincent87720 <vincent87720@yahoo.com.tw>
2023-01-04 20:07:15 +0800

    Feat: modify vuex

--- web/src/vuex/store.js
+++ web/src/vuex/store.js
@@ -13,7 +13,9 @@ const store = new Vuex.Store({
     systemConfigs: {},
     tradingSettings: {},
     products: [],
+    allProducts: [],
     suppliers: [],
+    allSuppliers: [],
   },
   mutations: {
     // 將state設定為參數
@@ -26,8 +28,14 @@ const store = new Vuex.Store({
     setProducts(state, payload) {
       state.products = payload;
     },
+    setAllProducts(state, payload) {
+      state.allProducts = payload;
+    },
     setSuppliers(state, payload) {
       state.suppliers = payload;
+    },
+    setAllSuppliers(state, payload) {
+      state.allSuppliers = payload;
     },
   },
   actions: {
@@ -52,36 +60,53 @@ const store = new Vuex.Store({
     },
     async GetProducts({ commit }) {
       let products = [];
+      let allProducts = [];
       await getProducts()
         .then((response) => {
           if (response.data.records != null) {
             response.data.records.map(function (x) {
-              products.push({
+              allProducts.push({
                 key: x.ID,
-                value: x.SKU + " " + x.Name,
+                value: x.Name,
+                SKU: x.SKU,
               });
+              if(x.DataStatus == 1){
+                products.push({
+                  key: x.ID,
+                  value: x.SKU+" "+x.Name,
+                });
+              }
             });
           }
         })
         .catch((error) => {});
       commit("setProducts", products);
+      commit("setAllProducts", allProducts);
     },
     async GetSuppliers({ commit }) {
       let suppliers = [];
+      let allSuppliers = [];
       await getSuppliers()
         .then((response) => {
           if (response.data.records != null) {
             response.data.records.map(function (x) {
-              suppliers.push({
+              allSuppliers.push({
                 key: x.ID,
                 value: x.Name,
               });
+              if(x.DataStatus == 1){
+                suppliers.push({
+                  key: x.ID,
+                  value: x.Name,
+                });
+              }
             });
           }
         })
         .catch((error) => {
         });
       commit("setSuppliers", suppliers);
+      commit("setAllSuppliers", allSuppliers);
     },
   },
 });
