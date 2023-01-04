c19e54009186e54fac908efb5b0f480c902032e1
vincent87720 <vincent87720@yahoo.com.tw>
2023-01-04 23:44:03 +0800

    Feat: add deliveryOrder routers

--- app/internal/routers/deliveryOrderRouters.go
+++ app/internal/routers/deliveryOrderRouters.go
@@ -2,11 +2,7 @@ package routers
 
 import (
 	"database/sql"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"net/http"
-	"regexp"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
@@ -14,94 +10,25 @@ import (
 )
 
 func SetupDeliveryOrderRouters(router *gin.Engine, db *sql.DB) (*gin.Engine, error) {
-
-	//從EasyStore及資料庫取得訂單資訊
-	router.GET("/deliveryorders/:id", GetEasyPickOrderHandler(db))
-	router.POST("/deliveryorders", PostDeliveryOrderHandler(db)) //減少庫存
-	// router.GET("/products", GetProductHandler(db))
-	// router.DELETE("/products/:id", DeleteProductHandler(db))
+	router.GET("/deliveryOrders", GetDeliveryOrdersHandler(db))
+	router.GET("/deliveryOrders/:id", GetDeliveryOrderHandler(db))
+	router.POST("/deliveryOrders", PostDeliveryOrderHandler(db))
+	router.PUT("/deliveryOrders/:id", PutDeliveryOrderHandler(db))
+	router.DELETE("/deliveryOrders/:id", DeleteDeliveryOrderHandler(db))
 
 	return router, nil
 }
-
-type JSONItems struct {
-	DeliveryProductName string `json:"product_name"`
-	NtdRetailPrice      string `json:"price"`
-	ProductSku          string `json:"sku"`
-	DeliveryProductQty  int    `json:"quantity"`
-}
-
-type JSONOrder struct {
-	DeliveryOrderID    int         `json:"id"`
-	DeliveryOrderTotal string      `json:"total_price"`
-	Products           []JSONItems `json:"items"`
-}
-
-type JSONData struct {
-	Order JSONOrder `json:"order"`
-}
-
-type JSONRAW struct {
-	Data JSONData `json:"data"`
-}
-
-func GetEasyPickOrderHandler(db *sql.DB) gin.HandlerFunc {
+func GetDeliveryOrdersHandler(db *sql.DB) gin.HandlerFunc {
 	fn := func(context *gin.Context) {
-		deliveryOrderID := context.Param("id")
-		resp, err := http.Get("http://127.0.0.1:5000/easystoreorders/" + deliveryOrderID)
-		if err != nil {
-			fmt.Println(err.Error())
-			context.JSON(http.StatusBadRequest, gin.H{
-				"status":  "FAIL",
-				"code":    "DERR0",
-				"message": "Can't connect to EasyStore.",
-			})
-			return
-		}
-
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println(err)
-			context.JSON(http.StatusBadRequest, gin.H{
-				"status": "FAIL",
-			})
-			return
-		}
-
-		var data JSONRAW
-
-		err = json.Unmarshal(body, &data)
-		if err != nil {
-			fmt.Println(err)
-			context.JSON(http.StatusBadRequest, gin.H{
-				"status": "FAIL",
-			})
-			return
-		}
-
-		deliveryOrder, errInfo, err := organizeData(db, data)
-		if err != nil {
-			fmt.Println(err)
-			context.JSON(http.StatusBadRequest, gin.H{
-				"status":  "FAIL",
-				"code":    errInfo.Code,
-				"message": errInfo.Message,
-			})
-			return
-		}
-
-		//檢查productIDXi陣列長度，小於等於零則不能進入GetProducts
-		if len(deliveryOrder.Products) <= 0 {
-			context.JSON(http.StatusOK, gin.H{
-				"status": "OK",
-				"order":  "[]",
-			})
+		deliveryOrderXi, modelErr := model.GetAllDeliveryOrders(db)
+		if modelErr != nil {
+			context.JSON(http.StatusBadRequest, modelError(modelErr))
 			return
 		}
 
 		context.JSON(http.StatusOK, gin.H{
-			"status": "OK",
-			"order":  deliveryOrder,
+			"status":  "OK",
+			"records": deliveryOrderXi,
 		})
 		return
 	}
@@ -109,97 +36,130 @@ func GetEasyPickOrderHandler(db *sql.DB) gin.HandlerFunc {
 	return gin.HandlerFunc(fn)
 }
 
-func organizeData(db *sql.DB, easyStoreData JSONRAW) (model.DeliveryOrder, *model.ModelError, error) {
+func GetDeliveryOrderHandler(db *sql.DB) gin.HandlerFunc {
+	fn := func(context *gin.Context) {
+		deliveryOrderID := context.Param("id")
 
-	var deliveryOrder model.DeliveryOrder
+		if checkEmpty(deliveryOrderID) == true {
+			context.JSON(http.StatusBadRequest, emptyError("id"))
+			return
+		}
 
-	//將從EasyStore取得的價格轉型後放入deliveryOrder.EasyStoreOrderTotal
-	totalPriceFloat, err := strconv.ParseFloat(easyStoreData.Data.Order.DeliveryOrderTotal, 32)
-	if err != nil {
-		fmt.Println(err)
-	}
-	deliveryOrder.EasyStoreOrderTotal = float32(totalPriceFloat)
-
-	//將從EasyStore取得的訂單編號轉型後放入deliveryOrder.DeliveryOrderID
-	deliveryOrder.DeliveryOrderID = strconv.Itoa(easyStoreData.Data.Order.DeliveryOrderID)
-
-	//將從EasyStore取得的訂單商品資訊放入deliveryOrder.Products
-	for _, val := range easyStoreData.Data.Order.Products {
-		priceFloat, err := strconv.ParseFloat(val.NtdRetailPrice, 32)
+		deliveryOrderIDVal, err := strconv.ParseInt(deliveryOrderID, 10, 64)
 		if err != nil {
-			fmt.Println(err)
+			context.JSON(http.StatusBadRequest, typeError("id"))
+			return
 		}
-		p := model.DeliveryProduct{
-			ProductSku:          val.ProductSku,
-			DeliveryProductName: val.DeliveryProductName,
-			NtdSellingPrice:     float32(priceFloat),
-			DeliveryProductQty:  val.DeliveryProductQty,
+		deliveryOrder := model.DeliveryOrder{
+			ID: deliveryOrderIDVal,
 		}
-		deliveryOrder.Products = append(deliveryOrder.Products, p)
+
+		deliveryOrderXi, modelErr := deliveryOrder.GetDeliveryOrder(db)
+		if modelErr != nil {
+			context.JSON(http.StatusBadRequest, modelError(modelErr))
+			return
+		}
+
+		context.JSON(http.StatusOK, gin.H{
+			"status":  "OK",
+			"records": deliveryOrderXi,
+		})
+		return
 	}
 
-	var productSkuXi []string
-
-	//檢查productID是否只能是[A-Za-z0-9_-]，不滿足規則便將其移除，滿足規則將其加入productStrXi
-	for idx, val := range deliveryOrder.Products {
-		r, _ := regexp.Compile(`[\w\-]+`)
-		if r.MatchString(val.ProductSku) == false {
-			deliveryOrder.Products = append(deliveryOrder.Products[:idx], deliveryOrder.Products[idx+1:]...)
-		} else {
-			productSkuXi = append(productSkuXi, val.ProductSku)
-		}
-	}
-
-	//依照productStrXi裡面的字串在資料庫內查詢對應資訊
-	// productMap, err := model.GetProducts(db, productSkuXi)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return deliveryOrder, &model.ModelError{Code: 0, Message: "?"}, err
-	// }
-	// if len(productSkuXi) != len(productMap) {
-
-	// 	return deliveryOrder, &model.ModelError{Code: 1, Message: "Some products not found in database."}, errors.New("Some products not found in database.")
-	// }
-
-	// for idx, val := range deliveryOrder.Products {
-	// 	//將查詢到的NtdRetailPrice放入deliveryOrder中
-	// 	deliveryOrder.Products[idx].NtdSellingPrice = productMap[val.ProductSku].NtdSellingPrice
-
-	// 	//計算並記錄商品每個商品的小計
-	// 	retailPrice := deliveryOrder.Products[idx].NtdSellingPrice
-	// 	qty := deliveryOrder.Products[idx].DeliveryProductQty
-	// 	deliveryOrder.Products[idx].DeliveryProductSubtotal = retailPrice * float32(qty)
-
-	// 	//累加每個商品的小計，計算總價
-	// 	deliveryOrder.DeliveryOrderTotal += deliveryOrder.Products[idx].DeliveryProductSubtotal
-	// }
-
-	// //將總價減去從EasyStore取得的價格，用於計算折扣
-	// deliveryOrder.Discount = deliveryOrder.DeliveryOrderTotal - deliveryOrder.EasyStoreOrderTotal
-
-	return deliveryOrder, nil, nil
+	return gin.HandlerFunc(fn)
 }
 
 func PostDeliveryOrderHandler(db *sql.DB) gin.HandlerFunc {
 	fn := func(context *gin.Context) {
 
-		type JsonRequest struct {
-			Order model.DeliveryOrder `json:"order"`
-		}
+		deliveryOrder := model.DeliveryOrder{}
 
-		var json JsonRequest
-		if err := context.ShouldBindJSON(&json); err != nil {
-			context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		err := context.BindJSON(&deliveryOrder)
+		if err != nil {
+			context.JSON(http.StatusBadRequest, typeError(err.Error()))
 			return
 		}
 
-		err := json.Order.DecreaseStocks(db)
+		modelErr := deliveryOrder.Create(db)
+		if modelErr != nil {
+			context.JSON(http.StatusBadRequest, modelError(modelErr))
+			return
+		}
+
+		context.JSON(http.StatusOK, gin.H{
+			"status": "OK",
+		})
+		return
+	}
+
+	return gin.HandlerFunc(fn)
+}
+
+func PutDeliveryOrderHandler(db *sql.DB) gin.HandlerFunc {
+	fn := func(context *gin.Context) {
+		supplierID := context.Param("id")
+
+		if checkEmpty(supplierID) == true {
+			context.JSON(http.StatusBadRequest, emptyError("id"))
+			return
+		}
+
+		supplierIDVal, err := strconv.ParseInt(supplierID, 10, 64)
 		if err != nil {
-			context.JSON(http.StatusBadRequest, gin.H{
-				"status":  "FAIL",
-				"code":    "DERR2",
-				"message": "Transaction fail.",
-			})
+			context.JSON(http.StatusBadRequest, typeError("id"))
+			return
+		}
+
+		deliveryOrderForm := model.DeliveryOrder{}
+
+		err = context.BindJSON(&deliveryOrderForm)
+		if err != nil {
+			context.JSON(http.StatusBadRequest, typeError(err.Error()))
+			return
+		}
+
+		deliveryOrder := deliveryOrderForm
+		deliveryOrder.ID = supplierIDVal
+
+		modelErr := deliveryOrder.Update(db)
+		if modelErr != nil {
+			context.JSON(http.StatusBadRequest, modelError(modelErr))
+			return
+		}
+
+		context.JSON(http.StatusOK, gin.H{
+			"status": "OK",
+		})
+		return
+	}
+
+	return gin.HandlerFunc(fn)
+}
+
+func DeleteDeliveryOrderHandler(db *sql.DB) gin.HandlerFunc {
+	fn := func(context *gin.Context) {
+
+		deliveryOrderID := context.Param("id")
+
+		if checkEmpty(deliveryOrderID) == true {
+			context.JSON(http.StatusBadRequest, emptyError("id"))
+			return
+		}
+
+		deliveryOrderIDVal, err := strconv.ParseInt(deliveryOrderID, 10, 64)
+		if err != nil {
+			context.JSON(http.StatusBadRequest, typeError("id"))
+			return
+		}
+
+		deliveryOrder := model.DeliveryOrder{
+			ID: deliveryOrderIDVal,
+		}
+
+		modelErr := deliveryOrder.Delete(db)
+		if modelErr != nil {
+			context.JSON(http.StatusBadRequest, modelError(modelErr))
 			return
 		}
 
