cf68b371edb371b9c7dfc8afa683fa2430bc77e2
vincent87720 <vincent87720@yahoo.com.tw>
2023-01-29 14:23:59 +0800

    Refactor: Use routing to distinguish between the main page and the login page

---
+++ web/src/pages/Main/Main.vue
@@ -0,0 +1,80 @@
+<template>
+    <v-app>
+        <v-app-bar app flat class="appbar">
+            <div class="pa-3 ml-auto mr-auto">
+                <img src="@/assets/8467417.png" width="120" />
+            </div>
+        </v-app-bar>
+        <v-main>
+            <v-window v-model="onboarding">
+                <v-window-item key="0">
+                    <Purchases />
+                </v-window-item>
+                <v-window-item key="1">
+                    <Products />
+                </v-window-item>
+                <v-window-item key="2">
+                    <Reports />
+                </v-window-item>
+                <v-window-item key="3">
+                    <Suppliers />
+                </v-window-item>
+                <v-window-item key="4">
+                    <DeliveryOrders />
+                </v-window-item>
+            </v-window>
+        </v-main>
+        <v-bottom-navigation v-model="onboarding" dark grow app>
+            <v-btn x-large>
+                <span>進貨</span>
+                <v-icon>mdi-package-down</v-icon>
+            </v-btn>
+            <v-btn x-large>
+                <span>商品管理</span>
+                <v-icon>mdi-package-variant-closed</v-icon>
+            </v-btn>
+            <v-btn x-large>
+                <span>報表</span>
+                <v-icon>mdi-chart-bar</v-icon>
+            </v-btn>
+            <v-btn x-large>
+                <span>廠商管理</span>
+                <v-icon>mdi-store-outline</v-icon>
+            </v-btn>
+            <v-btn x-large>
+                <span>出貨</span>
+                <v-icon>mdi-package-up</v-icon>
+            </v-btn>
+        </v-bottom-navigation>
+    </v-app>
+</template>
+
+<script>
+import Products from '@/pages/Products/Products.vue'
+import Suppliers from '@/pages/Suppliers/Suppliers.vue'
+import Purchases from '@/pages/Purchases/Purchases.vue'
+import DeliveryOrders from '@/pages/DeliveryOrders/DeliveryOrders.vue'
+import ProductDialog from '@/pages/Products/ProductDialog.vue'
+import Reports from '@/pages/Reports/index.vue'
+
+export default {
+    name: 'Main',
+    components: {
+        Products,
+        Suppliers,
+        Purchases,
+        DeliveryOrders,
+        ProductDialog,
+        Reports,
+    },
+    data: () => ({
+        onboarding: 0,
+    }),
+    created() {
+        this.$store.dispatch("GetSystemConfig");
+        this.$store.dispatch("GetTradingSettings");
+    },
+    methods: {
+    },
+};
+</script>
--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,14 +1,46 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import store from "@/vuex/store";
 
 Vue.use(VueRouter);
 
-const routes = [];
+const routes = [
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("@/pages/Authentication/Login.vue"),
+  },
+  {
+    path: "/main",
+    name: "Main",
+    component: () => import("@/pages/Main/Main.vue"),
+    meta: { requireAuth: true },
+  },
+];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach(async (to, from, next) => {
+  // console.log(from)
+
+  if (to.meta.requireAuth) {
+    if (store.state.auth.isLogin == true) {
+      next();
+    } else {
+      next({ name: "Login" });
+    }
+  } else {
+    if(to.name == "Login" && store.state.auth.isLogin == true){
+      next({ name: "Main" });
+    }
+    else{
+      next();
+    }
+  }
 });
 
 export default router;
